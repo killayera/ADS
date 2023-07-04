6b02ad822fc685e35075788c16749b0ab394ba36
killayera <130810825+killayera@users.noreply.github.com>
2023-07-04 16:23:53 +0600

    example

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void cnt(String[] args) {
+        int a = 5;
+        int b = 3;
+
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
