48f92db01d48f95f450b1111e80b215ff08829e6
killayera <130810825+killayera@users.noreply.github.com>
2023-07-05 13:20:53 +0600

    4th task, recursion

---
+++ FactorialCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static int factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+
+        int result = factorial(number);
+        System.out.println(result);
+
+        input.close();
+    }
+}
