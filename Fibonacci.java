d4871e7c982322c44aee4e4a63d04b4c460b6df5
killayera <130810825+killayera@users.noreply.github.com>
2023-07-06 15:03:39 +0600

    5th task fibonacci sequence

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        int n = input.nextInt();
+
+        int result = fibonacci(n);
+        System.out.println(result);
+
+        input.close();
+    }
+}
